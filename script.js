dbfa6186e1f332ae9304634cca82591425a4d9d8
AlinaD-2912 <dvornyk.alina2912@gmail.com>
2025-04-29 12:23:41 +0200

    dropdown menu style ajouté

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// Dropdown menu
+/*-- DROPDOWN MENU --*/
 
 function toggleDropdown() {
     //prend le boutton du dropdown par son id
@@ -21,11 +21,33 @@ function addDropdownItems() {
         const link = document.createElement('a');
         link.href = item.href;
         link.textContent = item.name;
-
         if (dropdown) dropdown.appendChild(link);
 
     });
 }
-
 // load
-window.addEventListener("DOMContentLoaded", addDropdownItems);
+window.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("dropdownButton");
+    if (button) {
+        button.innerHTML = `
+            <div style="display: flex; align-items: center; justify-content: space-between; width: 100%; gap: 10px;">
+                <div style="display: flex; align-items: center;">
+                    <img src="images/left-align.png" alt="Dropdown menu" style="height:30px;">
+                    <span style="margin-left: 8px;">StraySpawn</span>
+                </div>
+                <img src="images/cat.png" alt="Site Logo" style="height:30px; ">
+            </div>
+        `;
+    }
+
+    addDropdownItems();
+});
+
+/*-- FEED --*/
+const gamesFeed = [
+    {
+        nomDeJeu: 'Subnautica 2',
+        description: 'Un nouveau monde vous attend, aux côtés d’une histoire totalement inédite. Vous êtes un pionnier voyageant vers un monde lointain, mais quelque chose ne tourne pas rond. ',
+        image: ''
+    }
+]
